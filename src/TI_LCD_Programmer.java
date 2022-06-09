273435a91e8f7fb6beec8be80142dd1c88d32a1b
梅家诚 <1468856453@qq.com>
2022-06-10 01:15:21 +0800

    修复MIX模式中显示小数点的问题 修复Single模式下按左右移动键报错的问题

--- src/TI_LCD_Programmer.java
+++ src/TI_LCD_Programmer.java
@@ -308,7 +308,8 @@ public class TI_LCD_Programmer extends JFrame
             {
                 if(OverFlow.getText().equals("WARNING:CANNOT DIVIDE 0"))
                     OverFlow.setText("");
-                onDotandBit();
+                if(OperatingMode==0)
+                    onDotandBit();
                 if (OperatingMode == 0)
                 {
                     OperationLabel.setText("+");
@@ -343,7 +344,8 @@ public class TI_LCD_Programmer extends JFrame
             {
                 if(OverFlow.getText().equals("WARNING:CANNOT DIVIDE 0"))
                     OverFlow.setText("");
-                onDotandBit();
+                if(OperatingMode==0)
+                    onDotandBit();
                 if (OperatingMode == 0)
                 {
                     OperationLabel.setText("-");
@@ -378,7 +380,8 @@ public class TI_LCD_Programmer extends JFrame
             {
                 if(OverFlow.getText().equals("WARNING:CANNOT DIVIDE 0"))
                     OverFlow.setText("");
-                onDotandBit();
+                if(OperatingMode==0)
+                    onDotandBit();
                 if (OperatingMode == 0)
                 {
                     OperationLabel.setText("×");
@@ -413,7 +416,8 @@ public class TI_LCD_Programmer extends JFrame
             {
                 if(OverFlow.getText().equals("WARNING:CANNOT DIVIDE 0"))
                     OverFlow.setText("");
-                onDotandBit();
+                if(OperatingMode==0)
+                    onDotandBit();
                 if (OperatingMode == 0)
                 {
                     OperationLabel.setText("÷");
@@ -901,19 +905,23 @@ public class TI_LCD_Programmer extends JFrame
         KButton.registerKeyboardAction(e -> KButton.doClick(), KeyStroke.getKeyStroke(KeyEvent.VK_K, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
         IOput.registerKeyboardAction(e ->
         {
-            if (tmp.length() - 8 - index > 0)
-                index++;
-            resetIndex = true;
-            isEqualOperator = false;
-            displayIOput("");
+            if(OperatingMode==1) {
+                if (tmp.length() - 8 - index > 0)
+                    index++;
+                resetIndex = true;
+                isEqualOperator = false;
+                displayIOput("");
+            }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
         IOput.registerKeyboardAction(e ->
         {
-            if (index > 0)
-                index--;
+            if (OperatingMode == 1){
+                if (index > 0)
+                    index--;
             resetIndex = true;
             isEqualOperator = false;
             displayIOput("");
+        }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 
     }
